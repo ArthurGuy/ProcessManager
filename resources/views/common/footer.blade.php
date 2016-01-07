cf7b3577ab990de2eb12c31212b6f1cce48dba8e
Arthur Guy <arthur@assys.net>
2016-01-07 01:07:21 +0000

    Initial Commit, user logins

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,18 @@
+<div style="margin-top: 125px;">
+    <!-- Push Footer -->
+</div>
+
+<footer class="footer">
+    <div class="container" style="color: #777;">
+        <!-- Company Information -->
+        <div class="pull-left" style="padding-top: 28px;">
+            Copyright © Vestd
+        </div>
+
+        <div class="pull-right footer-social-icons">
+            &nbsp;
+        </div>
+
+        <div class="clearfix"></div>
+    </div>
+</footer>
