176322f72744cb8071f87143705d9239fbf7e179
Arthur Guy <arthur@assys.net>
2016-02-04 09:19:36 +0000

    removed the old db creation and rollback commands from the test script

--- test.sh
+++ test.sh
@@ -1,13 +1,4 @@
 set -e
 
-echo 'Creating the test sqlite db'
-touch storage/app/database.sqlite
-
-
 echo 'Running PHPUnit tests'
 vendor/bin/phpunit -c phpunit.xml --log-junit=junit.xml
-
-
-echo 'Testing database migration rollback'
-php artisan migrate:reset --database=sqlite --force
-
